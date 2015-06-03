f97c0aeb039f82af53d5c9af972bda105f99bd2e
photo-bro <photigragraphy@gmail.com>
2015-06-02 17:38:06 -0700

    Reimplemented [, ] functions. Fully works now

--- src/Brainterpreter.java
+++ src/Brainterpreter.java
@@ -1,18 +1,58 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 
 public class Brainterpreter {
 
-	
-	public static Parser c_P = Parser.getInstance();
-	
-	
-	
-	public static void main(String[] args) {
-		
-		String s = "+++.[.-]>"; // should print: 3 3 2 1
+	/* Description: Open and read BrainF*** source from file
+	 * @param			None
+	 * @return	String	String containing BrainF*** source
+	 */
+	public static String PromptFile() throws IOException{
+		// User input
+		Scanner in  = new Scanner(System.in);
+
+		// Prompt user for file path
+		System.out.printf("Enter path of BrainF*** file to interpret: ");
+		String path = in.nextLine();
+
+		// Open file
+		BufferedReader br = new BufferedReader(new FileReader(path));
+		String s = "";
+
+		// Try to read from file
+		try {
+			StringBuilder sb = new StringBuilder();
+			String l = br.readLine();
+			// Build file into string
+			while (l != null){
+				sb.append(l);
+				l = br.readLine();
+			}
+			s = sb.toString();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} finally{
+			br.close();
+		}
+
+		// Return contents of file as string
+		return s;
+	}
+
+
+	public static void main(String[] args) throws IOException {
+
+		Interpreter BF_Interpret = new Interpreter();
+
 		String hw = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
-		
-		c_P.ParseTape(hw);
-		
+
+
+		BF_Interpret.InterpretTape(PromptFile());
+
 	}
 
 }
---
+++ src/Interpreter.java
@@ -0,0 +1,126 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+
+public class Interpreter {
+
+	// Class data
+	private String m_Tape;		// Tape, program
+	private byte[] m_Mem;		// Program memory
+	private Scanner m_Scanner;	// For user input
+
+	/* Description: Default constructor. 
+	 * @param		N/A
+	 * @return		N/A
+	 */
+	public Interpreter(){
+		m_Mem = new byte[30000];
+		for (int i = 0; i < m_Mem.length; i++) m_Mem[i] = 0;
+		// Setup input
+		m_Scanner = new Scanner(System.in);
+
+	}
+
+	/* Description: Parses and interprets BrainF*** code. Print commands are
+	 *   sent to command line interface
+	 * @param	tape	A String containing proper BrainF*** source code
+	 * @return			None. Output is sent to command line
+	 */
+	public void InterpretTape(String tape){
+
+		m_Tape = tape;		
+		int dp = 0;			// data pointer
+		int ip = 0;			// instruction pointer
+		// For keeping track of starting loop indexes
+		Stack<Integer> loopStack = new Stack<Integer>(); 
+
+		// Parse through each item in tape
+		for(; ip < m_Tape.length(); ++ip){
+
+			switch(m_Tape.charAt(ip)){
+			case '>':
+				// Increment data pointer
+				++dp;
+				break;
+			case '<':
+				// Decrement data pointer
+				--dp;
+				break;
+			case '+':
+				// Increment value at data pointer
+				++m_Mem[dp];
+				//System.out.printf("%d", p);
+				break;
+			case '-':
+				// Decrement value at data pointer
+				--m_Mem[dp];
+				break;
+			case '.':
+				// Output value at data pointer
+				System.out.printf("%s", (char) m_Mem[dp]);
+				break;	
+			case ',':
+				// Input value at data pointer
+				m_Mem[dp] = m_Scanner.nextByte();
+				break;
+			case '[':
+				// Begin loop
+
+				// Wikipedia entry:
+				//  if the byte at the data pointer is zero, then instead of moving the 
+				// instruction pointer forward to the next command, jump it forward to 
+				// the command after the matching ] command.
+
+				if(m_Mem[dp] == 0){
+					int bc = 1;			// bracket count
+					while (bc > 0){
+						char c = (char) m_Tape.charAt(++ip);	
+
+						if (c == '[') bc++;
+						else if (c == ']') bc--;
+					}
+				}
+
+				//				if (m_Mem[dp] == 0){
+				//					++ip; // point to instruction after '['
+				//					while (m_Tape.charAt(ip) != ']'){
+				//						// nested loops
+				//						if (m_Tape.charAt(ip) == '[') 
+				//							loopStack.push(ip); // Save ip to go back to
+				//						++ip;
+				//					}
+				//				}
+				//				else
+				//					loopStack.push(ip); // Save ip to go back to
+				break;
+			case ']':
+				// End loop
+
+				// Wikipedia entry:
+				//  if the byte at the data pointer is nonzero, then instead of moving the 
+				// instruction pointer forward to the next command, jump it back to the 
+				// command after the matching [ command.
+
+				
+				int bc = 1;			// bracket count
+				while (bc > 0){
+					//System.out.printf("\n%s\n", ); // trace
+					char c = (char) m_Tape.charAt(--ip);	
+
+					if (c == '[') bc--;
+					else if (c == ']') bc++;
+				}
+				--ip;
+
+
+				//				if (m_Mem[dp] != 0)
+				//					ip = loopStack.peek();
+				//				else
+				//					loopStack.pop();
+				break;
+			default:
+				// Ignore all other input
+			} // Switch
+		} // For
+	} // ParseTape
+} // Interpreter
